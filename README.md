fd11cdecf9b2d23bf9b901e86b23c4d16742e479
John Oxley <john.oxley@liquidtelecom.com>
2015-05-08 17:02:35 +0100

    Initial effort

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # splunk-appexplore
-Application Exploration Application
+## Application Exploration Application
+
+This is a Splunk app that has a system wide exported dashboard that shows the event types, search macros, field extractions and field aliases that are present in the current application.
+
+This is useful for exploring a new application
---
+++ build.ps1
@@ -0,0 +1,19 @@
+$packageName = "splunk-appexplore"
+$outputDir = "target"
+$spl = ".\$outputDir\$packageName.spl"
+$tar = "$packageName.tar"
+
+if(Test-Path $spl) {
+	echo Deleting $spl
+	rm $spl
+}
+
+cp README.md $packageName
+
+mkdir -Force $outputDir
+
+7z a -ttar $tar "@compressfiles.txt"
+7z a -tgzip $spl $tar
+rm $tar
+
+rm $packageName/README.md
